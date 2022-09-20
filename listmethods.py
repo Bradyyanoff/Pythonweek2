1101fffdeb4a4e606b13d72f8442ee3bf2d15d8f
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-09-20 12:10:02 -0400

    Initial commit

---
+++ listmethods.py
@@ -0,0 +1,21 @@
+courses = ["comp151", "comp152", "math130", "math120"]
+courses.append("comp250")
+print(courses)
+courses.insert(1, "comp143")
+print(courses)
+#courses.remove("comp250")
+#courses.pop(5)
+courses.pop(-1)
+print(courses)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+weekdays = ["Sunday", "Monday", "Tuesday", "wednesday", "Thurbday", "Friday", "Saturday"]
+weekdays[-3] = "Thursday"
+print(weekdays)
+print(weekdays[-8])   # list index error
+
