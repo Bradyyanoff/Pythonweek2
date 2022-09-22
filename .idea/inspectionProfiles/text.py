d422ab684cee964af9b3251eb1092e9b2984ae8d
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-09-22 11:23:38 -0400

    opened file and printing content)

--- .idea/inspectionProfiles/text.py
+++ .idea/inspectionProfiles/text.py
@@ -2,5 +2,6 @@ courses_file = open("requiredCS (1).txt", 'r')
 lines_from_file = courses_file.readlines()
 for line in lines_from_file:
     loud_line = line.upper()
+    loud_line = loud_line.strip()
     print(loud_line)
 print("those are thew classes you HAVE to take for the CS Major")
