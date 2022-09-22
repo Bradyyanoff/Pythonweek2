e9b0a2cf5c01884e8c097fc0d53966b80d4cf9a1
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-09-22 11:40:31 -0400

    splitting the line

--- .idea/inspectionProfiles/text.py
+++ .idea/inspectionProfiles/text.py
@@ -1,7 +1,7 @@
 courses_file = open("requiredCS (1).txt", 'r')
 lines_from_file = courses_file.readlines()
 for line in lines_from_file:
-    loud_line = line.upper()
-    loud_line = loud_line.strip()
-    print(loud_line)
+    split = line.split(":")
+    print(split[0])
+    print(split[1])
 print("those are thew classes you HAVE to take for the CS Major")
